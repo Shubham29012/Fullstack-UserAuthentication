2d374dc30abb1243156aa25c4163fd0fcbda2164
Shubham29012 <sahil290120@gmail.com>
2025-04-30 02:47:23 +0530

    Switch to Render PostgreSQL

--- config/db-setup.js
+++ config/db-setup.js
@@ -2,14 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Required by Render for secure connection
+  }
+});
 
 const createUsersTable = `
   CREATE TABLE IF NOT EXISTS users (
